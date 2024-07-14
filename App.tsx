fa969206ddc357b18a19f8d739076dd23d8ff6be
Tỏn nè <oroki147@gmail.com>
2024-07-14 20:59:34 +0700

    abc

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Navigation from './src/components/navigation/Navigation';
 import {NavigationContainer} from '@react-navigation/native';
 import Menu from './src/components/products/screens/Menu';
-
+//////////////////////asdfasdfsadfsdfsa
 const App = () => {
   return (
     <NavigationContainer>
